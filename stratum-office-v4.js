464e3eef690ce964c33c02d0a040edd42ab6c7fa
stratumpraxis <stratumpraxis@gmail.com>
2026-09-11 05:11:17 +0900

    Pin v4 interaction layer to upgraded AI Office asset revision

--- stratum-office-v4.js
+++ stratum-office-v4.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   const s=document.createElement('script');
-  s.src='/preview/office.js';
+  s.src='/preview/office.js?v=139d8a4';
   s.onload=()=>document.dispatchEvent(new CustomEvent('stratum:v4ready'));
   s.onerror=()=>console.error('Stratum AI Office interaction layer failed to load.');
   document.head.appendChild(s);
